3a7b6f1bc7527b5be1567f47b56ee59da1af0e09
tp <61617660+ittp@users.noreply.github.com>
2022-07-10 22:01:11 +0300

    Create init.ps1

---
+++ windows/powershell/scripts/init.ps1
@@ -0,0 +1,9 @@
+# https://apps.ittps.ru/windows/powershell/scripts/init.ps1
+
+# Set-ExecutionPolicy Bypass -Scope Process -Force
+
+function init {
+  Write-Host "init script"
+}
+
+# iex ((New-Object System.Net.WebClient).DownloadString($link))
